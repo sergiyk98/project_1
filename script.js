c7b1a04541c5b510bdf5a2a59548cc50f73385ab
Sergiy <sergiy.kov0706@gmail.com>
2022-11-13 23:37:48 +0200

    1231

--- script.js
+++ script.js
@@ -33,10 +33,10 @@ const personalMovieDB = {
   privat: false,
 };
 
-const film1 = prompt('Один из последних просмотренных фильмов?', '');
-const rate1 = prompt('На сколько оцените его?', '');
-const film2 = prompt('Один из последних просмотренных фильмов?', '');
-const rate2 = prompt('На сколько оцените его?', '');
+const film1 = prompt('Один из последних просмотренных фильмов?', ''),
+  rate1 = prompt('На сколько оцените его?', ''),
+  film2 = prompt('Один из последних просмотренных фильмов?', ''),
+  rate2 = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[film1] = rate1;
 personalMovieDB.movies[film2] = rate2;
